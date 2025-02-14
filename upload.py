9d8e9ce9200a079fe094a2326d5da93b13873c67
DanYuDE <nadal3815@gmail.com>
2025-02-14 11:20:50 +0100

    Revise comments

--- upload.py
+++ upload.py
@@ -5,10 +5,10 @@ import config
 repo_id = "DanYuDE/llama-3.1-8B-dpo"  # Hugging Face Repository ID
 save_directory = config.fine_tuned_model_path  # model path
 
-# 加載 Fine-Tuned Model 和 Tokenizer
+# load fine-tuned model and tokenizer
 model = AutoModelForCausalLM.from_pretrained(save_directory)
 tokenizer = AutoTokenizer.from_pretrained(save_directory)
 
-# 上傳模型
+# push model and tokenizer to Hugging Face Hub
 model.push_to_hub(repo_id)
 tokenizer.push_to_hub(repo_id)
