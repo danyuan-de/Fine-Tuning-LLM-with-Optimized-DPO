5be1599a14e63f9fe22c6f5bb72eb9979fab5445
DanYuDE <nadal3815@gmail.com>
2025-02-28 11:59:27 +0100

    Update hyperparameter

--- config.py
+++ config.py
@@ -8,9 +8,9 @@ max_new_tokens = 512  # maximum number of tokens to generate
 batch_size = 4
 num_epochs = 1
 beta = 0.1
-learning_rate = 1e-5 # default is 5e-6, too small
-temperature = 0.7 # default is 0.3
-top_p = 0.8 # default is 0.9
+learning_rate = 1e-5 # try 5e-5, 5e-6 or 1e-6, etc.
+temperature = 0.7 # between 0.7 and 1.0, lower values generate more deterministic text
+top_p = 0.8 # between 0.7 and 0.95, higher values generate more diverse text
 
 file_content = "physics_qa_content.json"
 file_structure = "physics_qa_structure.json"
